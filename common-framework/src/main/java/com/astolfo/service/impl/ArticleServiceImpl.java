1db55c0527cd1dcf3609dd07af9e040d90874c3d
AstolfoNS <1780884916@qq.com>
2025-04-28 01:18:30 +0800

    修复了查询文章时查询不到tags的问题

--- common-framework/src/main/java/com/astolfo/service/impl/ArticleServiceImpl.java
+++ common-framework/src/main/java/com/astolfo/service/impl/ArticleServiceImpl.java
@@ -2,7 +2,6 @@ package com.astolfo.service.impl;
 
 import com.astolfo.common.constants.ArticleHomepageConstant;
 import com.astolfo.common.enums.ArticleSortField;
-import com.astolfo.common.enums.ArticleStatus;
 import com.astolfo.common.enums.HttpCode;
 import com.astolfo.common.result.PageResult;
 import com.astolfo.common.result.ResponseResult;
@@ -11,8 +10,6 @@ import com.astolfo.mapper.ArticleMapper;
 import com.astolfo.model.vo.ArticleDetailsVO;
 import com.astolfo.model.vo.ArticleSummaryVO;
 import com.astolfo.service.ArticleService;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import jakarta.annotation.Resource;
@@ -25,7 +22,7 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> impl
     private ArticleMapper articleMapper;
 
 
-    private Page<ArticleSummaryVO> createHomepageArticlePage(Integer page, Integer size) {
+    private Page<ArticleSummaryVO> setHomepageArticlePage(Integer page, Integer size) {
         if (page == null || page == 0) {
             page = ArticleHomepageConstant.DEFAULT_PAGE;
         }
@@ -42,10 +39,8 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> impl
             Integer size,
             ArticleSortField articleSortField
     ) {
-        Page<ArticleSummaryVO> articleSummaryVOPage = createHomepageArticlePage(page, size);
-
         Page<ArticleSummaryVO> resultPage =
-                articleMapper.getHomepageArticles(articleSummaryVOPage, articleSortField.getSortField());
+                articleMapper.getHomepageArticles(setHomepageArticlePage(page, size), articleSortField.getSortField());
 
         return ResponseResult.okResult(
                 PageResult
