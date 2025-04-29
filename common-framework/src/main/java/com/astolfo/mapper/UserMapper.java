400c8fe5cbaadb454fb65a91573298370e8f07e7
AstolfoNS <1780884916@qq.com>
2025-04-29 20:15:01 +0800

    对sql与语句进行了封装，抽离，复用。

--- common-framework/src/main/java/com/astolfo/mapper/UserMapper.java
+++ common-framework/src/main/java/com/astolfo/mapper/UserMapper.java
@@ -1,33 +1,20 @@
 package com.astolfo.mapper;
 
+import com.astolfo.mapper.provider.UserSqlProvider;
 import com.astolfo.model.entity.User;
 import com.astolfo.model.vo.UserVO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
 
-    @Select("""
-        SELECT
-            user.*
-        FROM
-            user
-        WHERE
-            user.id = #{id}
-    """)
+    @SelectProvider(type = UserSqlProvider.class, method = "getUserById")
     UserVO getUserVOById(@Param("id") Long id);
 
-    @Select("""
-        SELECT
-            user.*
-        FROM
-            user
-        WHERE
-            user.username = #{username}
-    """)
+    @SelectProvider(type = UserSqlProvider.class, method = "getUserByUsername")
     UserVO getUserVOByUsername(@Param("username") String username);
 
 }
