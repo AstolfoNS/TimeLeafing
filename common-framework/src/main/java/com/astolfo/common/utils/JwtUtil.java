61964c02d257ccb3770a0aa39bd281e409b978cb
AstolfoNS <1780884916@qq.com>
2025-04-30 20:35:08 +0800

    添加了jwtUtil工具类，用于解析jwt中的信息。

---
+++ common-framework/src/main/java/com/astolfo/common/utils/JwtUtil.java
@@ -0,0 +1,72 @@
+package com.astolfo.common.utils;
+
+import jakarta.annotation.Resource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtil {
+
+    @Resource
+    private JwtEncoder jwtEncoder;
+
+    @Resource
+    private JwtDecoder jwtDecoder;
+
+    @Value("${spring.security.jwt.expire}")
+    private Long expire;
+
+
+    private static List<String> getAuthoritiesFromUserDetails(Collection<? extends GrantedAuthority> authorities) {
+        return authorities
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+    }
+
+    public String generateToken(UserDetails userDetails, Instant issuedAt) {
+        JwtClaimsSet claims = JwtClaimsSet
+                .builder()
+                .subject(userDetails.getUsername())
+                .claim("authorities", getAuthoritiesFromUserDetails(userDetails.getAuthorities()))
+                .issuedAt(issuedAt)
+                .expiresAt(issuedAt.plusSeconds(expire))
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    public ParseToken parseToken(String token) throws JwtException {
+        return new ParseToken(jwtDecoder.decode(token));
+    }
+
+    @AllArgsConstructor
+    public static class ParseToken {
+
+        private final Jwt jwt;
+
+
+        public String getUsername() {
+            return jwt.getSubject();
+        }
+
+        public List<String> getAuthorities() {
+            return jwt.getClaimAsStringList("authorities");
+        }
+
+        public Boolean validate() {
+            return jwt.getExpiresAt() != null && jwt.getExpiresAt().isBefore(Instant.now());
+        }
+    }
+
+}
