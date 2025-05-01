3b2968bf6987c8cd8b9be4f823c948179bb605dc
AstolfoNS <1780884916@qq.com>
2025-05-01 13:58:32 +0800

    使用Bcrypt对用户代码进行加密，重写UserDetailsService。

--- common-framework/src/main/java/com/astolfo/security/entity/LoginUser.java
+++ common-framework/src/main/java/com/astolfo/security/entity/LoginUser.java
@@ -20,7 +20,7 @@ public class LoginUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(user.getRole().getRoleName());
+        return List.of();
     }
 
     @Override
