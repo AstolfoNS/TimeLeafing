77bccb102493b0c461ca3946a5f64d7ba4b51be5
AstolfoNS <1780884916@qq.com>
2025-05-02 23:32:56 +0800

    更换了RedisCacheUtil工具类。

--- common-framework/src/main/java/com/astolfo/common/utils/JacksonRedisSerializer.java
+++ common-framework/src/main/java/com/astolfo/common/utils/JacksonRedisSerializer.java
@@ -5,15 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
-import java.util.Objects;
-
 public class JacksonRedisSerializer<T> implements RedisSerializer<T> {
 
     private final ObjectMapper objectMapper;
-
     private final Class<T> type;
-
-
 
     public JacksonRedisSerializer(Class<T> type, ObjectMapper objectMapper) {
         this.type = type;
@@ -25,25 +20,25 @@ public class JacksonRedisSerializer<T> implements RedisSerializer<T> {
 
     @Override
     public byte[] serialize(T t) throws SerializationException {
-        if (Objects.isNull(t)) {
+        if (t == null) {
             return new byte[0];
         }
         try {
             return objectMapper.writeValueAsBytes(t);
         } catch (Exception e) {
-            throw new SerializationException("序列化失败", e);
+            throw new SerializationException("Error serializing object of type " + type.getName(), e);
         }
     }
 
     @Override
     public T deserialize(byte[] bytes) throws SerializationException {
-        if (Objects.isNull(bytes) || bytes.length == 0) {
+        if (bytes == null || bytes.length == 0) {
             return null;
         }
         try {
             return objectMapper.readValue(bytes, type);
         } catch (Exception e) {
-            throw new SerializationException("反序列化失败", e);
+            throw new SerializationException("Error deserializing to " + type.getName(), e);
         }
     }
 }
