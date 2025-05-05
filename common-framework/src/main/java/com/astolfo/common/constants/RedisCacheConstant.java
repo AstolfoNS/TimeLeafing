701f069bcf345a7f3a6b003ddd2ca7e2ab946985
AstolfoNS <1780884916@qq.com>
2025-05-05 17:59:55 +0800

    <login:userId, loginUser>存入redis中。

--- common-framework/src/main/java/com/astolfo/common/constants/RedisCacheConstant.java
+++ common-framework/src/main/java/com/astolfo/common/constants/RedisCacheConstant.java
@@ -2,6 +2,6 @@ package com.astolfo.common.constants;
 
 public class RedisCacheConstant {
 
-    public static final String NULL_PLACEHOLDER = "__null__";
+    public static final String Login_USER_PERFIX = "login:";
 
 }
