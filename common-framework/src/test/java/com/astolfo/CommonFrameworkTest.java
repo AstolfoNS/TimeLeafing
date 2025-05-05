c137770b8bc912b60b85ff995692b299374ae993
Astolfo <1780884916@qq.com>
2025-05-06 00:27:28 +0800

    测试。

--- common-framework/src/test/java/com/astolfo/CommonFrameworkTest.java
+++ common-framework/src/test/java/com/astolfo/CommonFrameworkTest.java
@@ -9,9 +9,6 @@ public class CommonFrameworkTest {
 
     @Test
     public void testBCryptPasswordEncoder() {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("456123"));
-        System.out.println(encoder.encode("123456"));
     }
 
 }
