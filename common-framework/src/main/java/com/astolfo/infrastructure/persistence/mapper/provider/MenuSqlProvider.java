16ebfda06d386eedc6cd997643081042b0a3cfed
Astolfo <1780884916@qq.com>
2025-05-11 23:21:41 +0800

    添加了Mapper中的sql语句。

--- common-framework/src/main/java/com/astolfo/infrastructure/persistence/mapper/provider/MenuSqlProvider.java
+++ common-framework/src/main/java/com/astolfo/infrastructure/persistence/mapper/provider/MenuSqlProvider.java
@@ -1,7 +1,36 @@
 package com.astolfo.infrastructure.persistence.mapper.provider;
 
+import org.apache.ibatis.annotations.Param;
+
 public class MenuSqlProvider {
 
-
+    String findMenuEntityListByUserUsername(@Param("username") String username) {
+        return """
+            SELECT
+                DISTINCT m.id, m.name, m.url, m.permission_string, m.parent_id, m.order_num
+            FROM
+                users u
+            JOIN
+                user_role ur
+            ON
+                u.id = ur.user_id
+            JOIN 
+                role r 
+            ON 
+                ur.role_id = r.id
+            JOIN 
+                role_menu rm 
+            ON 
+                r.id = rm.role_id
+            JOIN 
+                menu m 
+            ON 
+                rm.menu_id = m.id
+            WHERE 
+                u.username = #{username}
+            ORDER BY 
+                m.order_num ASC
+        """;
+    }
 
 }
