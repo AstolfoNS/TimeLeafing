c920775f30df4189327b0875242e29879ea4323e
Astolfo <1780884916@qq.com>
2025-05-11 22:49:44 +0800

    添加了Menu中List<Menu>和List<MenuEntity>的转换方法。

--- common-framework/src/main/java/com/astolfo/converter/MenuConverter.java
+++ common-framework/src/main/java/com/astolfo/converter/MenuConverter.java
@@ -7,43 +7,65 @@ import com.astolfo.domain.rbac.model.valueobject.enumtype.HttpMethod;
 import com.astolfo.infrastructure.persistence.entity.MenuEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Component
 public class MenuConverter {
 
-    public Optional<Menu> toDomain(MenuEntity menuEntity) {
-        return Optional.of(new Menu(
-                        menuEntity.getId(),
-                        Permission.of(menuEntity.getPermission()),
-                        menuEntity.getDescription(),
-                        menuEntity.getUrl(),
-                        HttpMethod.get(menuEntity.getPermission()),
-                        AuthorityType.get(menuEntity.getAuthorityType()),
-                        menuEntity.getOrderNum(),
-                        menuEntity.getEnabled(),
-                        menuEntity.getCreateTime(),
-                        menuEntity.getUpdateTime(),
-                        menuEntity.getIsDeleted()
-                )
-        );
+    public Menu toDomain(MenuEntity menuEntity) {
+        if (menuEntity == null) {
+            return null;
+        } else {
+            return new Menu(
+                    menuEntity.getId(),
+                    Permission.of(menuEntity.getPermission()),
+                    menuEntity.getDescription(),
+                    menuEntity.getUrl(),
+                    HttpMethod.get(menuEntity.getPermission()),
+                    AuthorityType.get(menuEntity.getAuthorityType()),
+                    menuEntity.getOrderNum(),
+                    menuEntity.getEnabled(),
+                    menuEntity.getCreateTime(),
+                    menuEntity.getUpdateTime(),
+                    menuEntity.getIsDeleted()
+            );
+        }
     }
 
-    public Optional<MenuEntity> toEntity(Menu menu) {
-        return Optional.of(new MenuEntity(
-                        menu.getId(),
-                        menu.getPermission().getPermissionName(),
-                        menu.getDescription(),
-                        menu.getUrl(),
-                        menu.getHttpMethod().getHttpMethodName(),
-                        menu.getAuthorityType().getAuthorityTypeName(),
-                        menu.getOrderNum(),
-                        menu.getEnabled(),
-                        menu.getCreateTime(),
-                        menu.getUpdateTime(),
-                        menu.getIsDeleted()
-                )
-        );
+    public MenuEntity toEntity(Menu menu) {
+        if (menu == null) {
+            return null;
+        } else {
+            return new MenuEntity(
+                    menu.getId(),
+                    menu.getPermission().getPermissionName(),
+                    menu.getDescription(),
+                    menu.getUrl(),
+                    menu.getHttpMethod().getHttpMethodName(),
+                    menu.getAuthorityType().getAuthorityTypeName(),
+                    menu.getOrderNum(),
+                    menu.getEnabled(),
+                    menu.getCreateTime(),
+                    menu.getUpdateTime(),
+                    menu.getIsDeleted()
+            );
+        }
+    }
+
+    public List<Menu> toDomain(List<MenuEntity> menuEntityList) {
+        return menuEntityList
+                .stream()
+                .map(menuEntity -> Optional.of(toDomain(menuEntity)).orElseThrow(() -> new NoSuchElementException("Can not convert null menuEntity")))
+                .toList();
+    }
+
+    public List<MenuEntity> toEntity(List<Menu> menuList) {
+        return menuList
+                .stream()
+                .map(menu -> Optional.of(toEntity(menu)).orElseThrow(() -> new NoSuchElementException("Can not convert null menu")))
+                .toList();
     }
 
 }
