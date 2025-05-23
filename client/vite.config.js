c758d563fc77c81143b97b1c54b860ff39d54b74
Astolfo <1780884916@qq.com>
2025-05-23 17:08:55 +0800

    初始化client模块

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,14 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
