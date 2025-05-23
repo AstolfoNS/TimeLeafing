c7b3289f07d5bcbabc440b5799edeb5bbe03549c
Astolfo <1780884916@qq.com>
2025-05-23 21:04:20 +0800

    重写了PermissionConverter。

--- common-framework/src/main/java/com/astolfo/infrastructure/persistence/converter/impl/PermissionConverterImpl.java
+++ common-framework/src/main/java/com/astolfo/infrastructure/persistence/converter/impl/PermissionConverterImpl.java
@@ -1,41 +1,88 @@
 package com.astolfo.infrastructure.persistence.converter.impl;
 
 import com.astolfo.domain.rbac.model.root.Permission;
+import com.astolfo.domain.rbac.model.valueobject.*;
 import com.astolfo.infrastructure.persistence.converter.PermissionConverter;
 import com.astolfo.infrastructure.persistence.entity.PermissionEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class PermissionConverterImpl implements PermissionConverter {
 
-
     @Override
     public Permission toDomain(PermissionEntity permissionEntity) {
-        // TODO: toDomain
+        Permission.Details details = new Permission.Details();
 
-        return null;
+        details.setId(PermissionId.of(permissionEntity.getId()));
+
+        details.setSymbol(Symbol.of(permissionEntity.getSymbol()));
+
+        details.setDescription(permissionEntity.getDescription());
+
+        details.setUrl(UrlPattern.of(permissionEntity.getUrl()));
+
+        details.setHttpMethod(HttpMethod.of(permissionEntity.getHttpMethod()));
+
+        details.setPoint(Point.of(permissionEntity.getPoint()));
+
+        details.setOrderNum(permissionEntity.getOrderNum());
+
+        details.setEnabled(permissionEntity.getEnabled());
+
+        details.setCreateTime(permissionEntity.getCreateTime());
+
+        details.setUpdateTime(permissionEntity.getUpdateTime());
+
+        details.setIsDeleted(permissionEntity.getIsDeleted());
+
+        return Permission.of(details);
     }
 
     @Override
     public List<Permission> toDomain(List<PermissionEntity> permissionEntityList) {
-        // TODO: toDomain
-
-        return List.of();
+        return permissionEntityList
+                .stream()
+                .map(this::toDomain)
+                .collect(Collectors.toList());
     }
 
     @Override
     public PermissionEntity toEntity(Permission permission) {
-        // TODO: toEntity
+        PermissionEntity permissionEntity = new PermissionEntity();
 
-        return null;
+        permissionEntity.setId(permission.getId().getPermissionId());
+
+        permissionEntity.setSymbol(permission.getSymbol().getSymbol());
+
+        permissionEntity.setDescription(permission.getDescription());
+
+        permissionEntity.setUrl(permission.getUrl().getUrlPattern());
+
+        permissionEntity.setHttpMethod(permission.getHttpMethod().getHttpMethod());
+
+        permissionEntity.setPoint(permission.getPoint().getPoint());
+
+        permissionEntity.setOrderNum(permission.getOrderNum());
+
+        permissionEntity.setEnabled(permission.getIsDeleted());
+
+        permissionEntity.setCreateTime(permission.getCreateTime());
+
+        permissionEntity.setUpdateTime(permission.getUpdateTime());
+
+        permissionEntity.setIsDeleted(permission.getIsDeleted());
+
+        return permissionEntity;
     }
 
     @Override
     public List<PermissionEntity> toEntity(List<Permission> permissionList) {
-        // TODO: toEntity
-
-        return List.of();
+        return permissionList
+                .stream()
+                .map(this::toEntity)
+                .collect(Collectors.toList());
     }
 }
